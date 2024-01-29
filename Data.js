fdc767d043deb41e9d6bc107558c38c6a2e33047
Eetu Purhonen <eetupurhonen8@gmail.com>
2024-01-29 10:04:32 +0200

    all files committed
    school task files committed

---
+++ Data.js
@@ -0,0 +1,54 @@
+export const DATA = [
+    {
+        id: '1',
+        lastname: 'Juntunen',
+        firstname: 'Jussi'
+    },
+    {
+        id: '2',
+        lastname: 'Koivisto',
+        firstname: 'Matti'
+    },
+    {
+        id: '3',
+        lastname: 'Mäkinen',
+        firstname: 'Pekka'
+
+
+    },
+    {
+        id: '4',
+        lastname: 'Korhonen',
+        firstname: 'Kalle'
+    },
+    {
+        id: '5',
+        lastname: 'Virtanen',
+        firstname: 'Jukka'
+    },
+    {
+        id: '6',
+        lastname: 'Nieminen',
+        firstname: 'Mikko'
+    },
+    {
+        id: '7',
+        lastname: 'Mäkelä',
+        firstname: 'Antti'
+    },
+    {
+        id: '8',
+        lastname: 'Hämäläinen',
+        firstname: 'Ilkka'
+    },
+    {
+        id: '9',
+        lastname: 'Laine',
+        firstname: 'Ville'
+    },
+    {
+        id: '10',
+        lastname: 'Heikkinen',
+        firstname: 'Joonas'
+    },
+]
